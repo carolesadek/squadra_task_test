6264cd8a2fef83585a750c6476a15565266647a5
Carole Sadek <s295590@studenti.polito.it>
2024-09-09 13:08:26 +0200

    main initialization

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        // This will print the message followed by a new line
+        System.out.println("Hello, World!");
+
+        // This will print the message without a new line
+        System.out.print("Hello, ");
+        System.out.print("World!");
+    }
+}
